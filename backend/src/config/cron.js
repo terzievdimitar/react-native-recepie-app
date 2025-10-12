6212490ff4b49ab0e642966af81ef7b517cfcf6e
Plo4i <midag@abv.bg>
2025-10-12 23:01:04 +0200

    feat(cron): update cron job schedule to run every 14 minutes

--- backend/src/config/cron.js
+++ backend/src/config/cron.js
@@ -1,7 +1,7 @@
 import cron from 'cron';
 import https from 'https';
 
-const job = new cron.CronJob('0 0 * * *', () => {
+const job = new cron.CronJob('*/14 * * * *', () => {
 	https.get(process.env.API_URL, (res) => {
 		if (res.statusCode === 200) {
 			console.log('GET request sent successfully');
